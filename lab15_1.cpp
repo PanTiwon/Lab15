a49ebd5da58bf7acdc5a86dea0b0511dcc53ddb5
PanTiwon <kkhowphuns@gmail.com>
2025-01-31 15:25:12 +0700

    Lab15_1 Complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,25 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int* x = &a;
+	char* y = &b;
+	int** z = &x;
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z << endl;
+	cout << (void *)&a << " " << (void *)&b << " " << (void *)&c << " " << (void *)&x << " " << (void *)&y << " " << (void *)&z << endl;
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " <<(void *) y << " " << z << endl;
+	b = 'W';
+	*y = b; 	
+	cout << a << " " << b << " " << c << " " << x << " " <<(void *) y << " " << z << endl;
+	a = 6;
+	*x = a;
+	cout << a << " " << b << " " << c << " " << x << " " <<(void *) y << " " << z << endl;
+	a = 7;
+	**z = a;
+	cout << a << " " << b << " " << c << " " << x << " " <<(void *) y << " " << z << endl;
 	return 0;
 }
